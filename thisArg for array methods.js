8f8b25dda53757a1da99357f04f5b5f49c6a9544
@shmulim <shmuel.markel@gmail.com>
2015-06-02 12:58:23 -0700

    Create thisArg for array methods.js

---
+++ thisArg for array methods.js
@@ -0,0 +1,12 @@
+var obj = {
+  a: 42,
+  b: 43
+};
+
+function arbitraryFn(arr){
+  return arr.map(function(i){
+    return this.a; //42
+  }, obj); // <-- defines the 'this' keyword
+}
+
+arbitraryFn([34, 42, 56]); // [42, 42, 42]
